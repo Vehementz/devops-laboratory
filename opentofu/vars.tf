85d073cd7804c5693d93b38aae1cbadc7b51be5c
Vehementz <vale-tudo91@sfr.fr>
2024-08-19 15:04:23 +0200

    standart edition

---
+++ opentofu/vars.tf
@@ -0,0 +1,11 @@
+variable "ssh_key" {
+  default = "ssh-rsa fakeH"
+}
+
+variable "proxmox_host" {
+    default = "prox-1u"
+}
+
+variable "template_name" {
+    default = "ubuntu-2004-cloudinit-template"
+}
